70750d95115b3c2f02df2d63af94d9f84343e232
narekkeryan <narek.keryan@gmail.com>
2018-04-25 19:09:49 +0400

    Cleaned code

--- handler.js
+++ handler.js
@@ -14,23 +14,16 @@ module.exports.createCountry = (event, context, callback) => {
     let body = JSON.parse(event.body);
 
     let data = [];
-    for (let i = 0; i < body.length; i++) {
-        data.push({ name: body[i].name, code: body[i].alpha2Code });
+    for (let bodyItem of body) {
+        data.push({ name: bodyItem.name, code: bodyItem.alpha2Code });
     }
 
     connectToDatabase()
-        .then(() => {
-            Country.create(data)
-                .then(countries => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(countries)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => Country.create(data))
+        .then(countries => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(countries)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
@@ -42,18 +35,11 @@ module.exports.getCountries = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
     connectToDatabase()
-        .then(() => {
-            Country.find()
-                .then(countries => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(countries)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => Country.find())
+        .then(countries => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(countries)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
@@ -65,39 +51,32 @@ module.exports.createCity = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
     connectToDatabase()
-        .then(() => {
-            Country.find({}, { code: 1 })
-                .then(countries => {
-                    let formatedCountries = {};
-                    countries.forEach(country => {
-                        formatedCountries[country.code] = country._id;
-                    });
+        .then(() => Country.find({}, { code: 1 }))
+        .then(countries => {
+            let formatedCountries = {};
+            countries.forEach(country => {
+                formatedCountries[country.code] = country._id;
+            });
 
-                    let data = [], cityNames = [];
-                    for (let i = 0; i < cities.length; i++) {
-                        if (cityNames.includes(cities[i].name)) {
-                            continue;
-                        }
-                        cityNames.push(cities[i].name);
-                        let zips = zipcodes.lookupByName(cities[i].name, cities[i].country);
-                        if (zips.length) {
-                            let postalCode = zips[0].zip;
-                            if (postalCode) {
-                                data.push({ name: cities[i].name, postalCode: postalCode, country: formatedCountries[cities[i].country] });
-                            }
-                        }
+            let data = [], cityNames = [];
+            for (let city of cities) {
+                if (cityNames.includes(city.name)) {
+                    continue;
+                }
+                cityNames.push(city.name);
+                let zips = zipcodes.lookupByName(city.name, city.country);
+                if (zips.length) {
+                    let postalCode = zips[0].zip;
+                    if (postalCode) {
+                        data.push({ name: city.name, postalCode: postalCode, country: formatedCountries[city.country] });
                     }
-                    City.create(data)
-                        .then(cities => callback(null, {
-                            statusCode: 200,
-                            body: JSON.stringify(cities)
-                        }))
-                        .catch(err => callback(null, {
-                            statusCode: err.statusCode || 500,
-                            headers: { 'Content-Type': 'text/plain' },
-                            body: err.message
-                        }));
-                })
+                }
+            }
+            City.create(data)
+                .then(cities => callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(cities)
+                }))
                 .catch(err => callback(null, {
                     statusCode: err.statusCode || 500,
                     headers: { 'Content-Type': 'text/plain' },
@@ -114,23 +93,12 @@ module.exports.createCity = (event, context, callback) => {
 module.exports.getCities = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
 
-    let params = event.queryStringParameters;
-    console.log(params);
-    // event.pathParameters.id
-
     connectToDatabase()
-        .then(() => {
-            City.find().populate('country').exec()
-                .then(cities => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(cities)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => City.find().populate('country').exec())
+        .then(cities => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(cities)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
@@ -144,18 +112,11 @@ module.exports.getCountriesByName = (event, context, callback) => {
     let name = event.pathParameters.name.replace(/%20/g, " ");
 
     connectToDatabase()
-        .then(() => {
-            Country.find({ name: { $regex: '.*' + name + '.*' } })
-                .then(countries => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(countries)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => Country.find({ name: { $regex: '.*' + name + '.*' } }))
+        .then(countries => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(countries)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
@@ -169,18 +130,11 @@ module.exports.getCitiesByName = (event, context, callback) => {
     let name = event.pathParameters.name.replace(/%20/g, " ");
 
     connectToDatabase()
-        .then(() => {
-            City.find({ name: { $regex: '.*' + name + '.*' } }).populate('country').exec()
-                .then(cities => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(cities)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => City.find({ name: { $regex: '.*' + name + '.*' } }).populate('country').exec())
+        .then(cities => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(cities)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
@@ -194,21 +148,14 @@ module.exports.getCitiesByCountryName = (event, context, callback) => {
     let name = event.pathParameters.name.replace(/%20/g, " ");
 
     connectToDatabase()
-        .then(() => {
-            Country.find({ name: { $regex: '.*' + name + '.*' } }, { _id: 1 })
-                .then(countries => {
-                    let ids = countries.map(country => country._id);
-                    City.find({ country: { $in: ids } }).populate('country').exec()
-                        .then(cities => callback(null, {
-                            statusCode: 200,
-                            body: JSON.stringify(cities)
-                        }))
-                        .catch(err => callback(null, {
-                            statusCode: err.statusCode || 500,
-                            headers: { 'Content-Type': 'text/plain' },
-                            body: err.message
-                        }));
-                })
+        .then(() => Country.find({ name: { $regex: '.*' + name + '.*' } }, { _id: 1 }))
+        .then(countries => {
+            let ids = countries.map(country => country._id);
+            City.find({ country: { $in: ids } }).populate('country').exec()
+                .then(cities => callback(null, {
+                    statusCode: 200,
+                    body: JSON.stringify(cities)
+                }))
                 .catch(err => callback(null, {
                     statusCode: err.statusCode || 500,
                     headers: { 'Content-Type': 'text/plain' },
@@ -228,18 +175,11 @@ module.exports.getCityByPostal = (event, context, callback) => {
     let zip = event.pathParameters.zip.replace(/%20/g, " ");
 
     connectToDatabase()
-        .then(() => {
-            City.find({ postalCode: zip }).populate('country').exec()
-                .then(city => callback(null, {
-                    statusCode: 200,
-                    body: JSON.stringify(city)
-                }))
-                .catch(err => callback(null, {
-                    statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
-                    body: err.message
-                }));
-        })
+        .then(() => City.find({ postalCode: zip }).populate('country').exec())
+        .then(city => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(city)
+        }))
         .catch(err => callback(null, {
             statusCode: err.statusCode || 500,
             headers: { 'Content-Type': 'text/plain' },
